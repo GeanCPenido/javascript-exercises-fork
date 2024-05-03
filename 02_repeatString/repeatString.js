d60539dfb0647c51374391cc14dd94cd9bcbf379
Penido <geanc.penido@gmail.com>
2024-05-03 18:29:10 -0300

    2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let str = "";
+    const number = Math.floor(Math.random() * 1000);
+
+    if (num < 0) return "ERROR";
+    for (i = 0; i < num; i++) {
+        str += string;
+    }
+    return str;
 
 };
 
