37caa7f3cfb2bf155da09829693aea93545c0c3f
Penido <geanc.penido@gmail.com>
2024-05-04 15:12:48 -0300

    7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  const toCelsius = (Fahrenheit - 32) * 5 / 9;
+  return Math.round(toCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+  return Math.round((Celsius * 9 / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
@@ -9,3 +12,7 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// var rounded = Math.round(number * 10) / 10
+// from F to C == x °F ≘ (x − 32) × 5/9 °C 
+// from C to F == x °C ≘ (x × 9/5 + 32) °F 
