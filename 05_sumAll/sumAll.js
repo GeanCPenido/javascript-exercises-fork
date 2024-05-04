f4498c7ab438277152d912317690d77d3baf97ee
Penido <geanc.penido@gmail.com>
2024-05-04 00:44:01 -0300

    5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    
+    return num1 < 0 || num2 < 0 ? 'ERROR' :
+    typeof num1 != 'number' || typeof num2 != 'number' ? 'ERROR' :
+    num1 < num2 ? num2 * (num1 + num2) / 2 :
+    num1 * (num2 + num1) / 2;
 
 };
-
+// n2(n1 + n2) / 2
 // Do not edit below this line
 module.exports = sumAll;
